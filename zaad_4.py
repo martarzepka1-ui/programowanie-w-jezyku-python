7555db038f4b5896d59c83fe1b150b9e3f911d4b
Marta <marta.rzepka1@edu.uekat.pl>
2025-12-27 15:34:32 +0100

    add zad_1, zaad_2, zaad_3, zaad_4, zaad_5, zaad_6

---
+++ zaad_4.py
@@ -0,0 +1,5 @@
+def spr(x:int, y:int, z:int) ->int:
+    return x+y>=z
+
+print(spr(3,6,20))
+
---
+++ zad_1.py
@@ -0,0 +1,6 @@
+def moja_funkcja(name:str, surname:str) -> str:
+    return f"Cześć {name} {surname}!"
+
+uruchomienie = moja_funkcja("Anna", "Nowak")
+
+print(uruchomienie)
