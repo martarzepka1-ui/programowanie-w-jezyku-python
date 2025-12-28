9fb5409ea174f815762abe5ad02f96907cd31272
Marta <marta.rzepka1@edu.uekat.pl>
2025-12-28 11:42:23 +0100

    poprawki pycodestyle PEP-8

--- zad_2a.py
+++ zad_2a.py
@@ -2,5 +2,6 @@ def wyswietl_imiona(imiona):
     for imie in imiona:
         print(imie)
 
-lista_imion=["Marta", "Martyna", "Laura", "Agnieszka", "Renata"]
+
+lista_imion = ["Marta", "Martyna", "Laura", "Agnieszka", "Renata"]
 wyswietl_imiona(lista_imion)
--- zad_2c.py
+++ zad_2c.py
@@ -3,5 +3,6 @@ def parzyste_liczby(liczby):
         if liczba % 2 == 0:
             print(liczba)
 
+
 liczby = list(range(10))
 parzyste_liczby(liczby)
